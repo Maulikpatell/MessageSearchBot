0600de40d4d0c12c040f72d480fab8693b695673
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2022-08-18 00:54:12 +0530

    Update database.py

--- TeamTeleRoid/database.py
+++ TeamTeleRoid/database.py
@@ -11,6 +11,7 @@ class Database:
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
         self.col = self.db.users
+        self.grp = self.db.groups
 
     def new_user(self, id):
         return dict(
@@ -23,13 +24,21 @@ class Database:
                 ban_reason=''
             )
         )
+    def new_group(self, id):
+        return dict(
+            id = id,
+            chat_status=dict(
+                is_disabled=False,
+                reason="",
+            ),
+        )
 
     async def add_user(self, id):
         user = self.new_user(id)
         await self.col.insert_one(user)
 
     async def is_user_exist(self, id):
-        user = await self.col.find_one({'id': int(id)})
+        user = await self.col.find_one({'id':int(id)})
         return True if user else False
 
     async def total_users_count(self):
